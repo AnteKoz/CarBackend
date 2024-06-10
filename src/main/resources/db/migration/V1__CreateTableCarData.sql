bf5b8650a9057d009ba3f76c5def37be8056206b
AnteKoz <Ante.Kozina@adesso.de>
2024-06-10 10:22:38 +0200

    feat(flyscript)!: adding Flyscript to initialize database in PostgreSQL image

---
+++ src/main/resources/db/migration/V1__CreateTableCarData.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS car(
+    id BIGINT PRIMARY KEY,
+    brand VARCHAR(255),
+    code VARCHAR(255),
+    description VARCHAR(255)
+);
---
+++ src/main/resources/db/migration/V2__TestData.sql
@@ -0,0 +1,8 @@
+insert into car(id, brand, code, description) values (1, 'Mercedes', '11111', 'Mercedes_11111_de');
+insert into car(id, brand, code, description) values (11, 'Mercedes', '11111', 'Mercedes_11111_en');
+insert into car(id, brand, code, description) values (12, 'Mercedes', '11111', 'Mercedes_11111_it');
+insert into car(id, brand, code, description) values (2, 'BMW', '99999', 'BMW_99999_en');
+insert into car(id, brand, code, description) values (3, 'Ford', '22222', 'Ford_22222_it');
+insert into car(id, brand, code, description) values (4, 'BMW', '88888', 'BMW_88888_de');
+insert into car(id, brand, code, description) values (5, 'Mercedes', '33333', 'Mercedes_33333_en');
+insert into car(id, brand, code, description) values (6, 'Ford', '44444', 'Ford_44444_de');
