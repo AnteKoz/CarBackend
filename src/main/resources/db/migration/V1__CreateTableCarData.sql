a257103ffdb9eb318fce95bd97e9f20cce39d2ae
AnteKoz <Ante.Kozina@adesso.de>
2024-06-13 09:29:06 +0200

    feat: Adjusted CarData Flyscript giles

--- src/main/resources/db/migration/V1__CreateTableCarData.sql
+++ src/main/resources/db/migration/V1__CreateTableCarData.sql
@@ -1,6 +1,5 @@
 CREATE TABLE IF NOT EXISTS car(
     id BIGINT PRIMARY KEY,
     brand VARCHAR(255),
-    code VARCHAR(255),
-    description VARCHAR(255)
+    code VARCHAR(255)
 );
--- src/main/resources/db/migration/V2__TestData.sql
+++ src/main/resources/db/migration/V2__TestData.sql
@@ -1,8 +1,8 @@
-insert into car(id, brand, code, description) values (1, 'Mercedes', '11111', 'Mercedes_11111_de');
-insert into car(id, brand, code, description) values (11, 'Mercedes', '11111', 'Mercedes_11111_en');
-insert into car(id, brand, code, description) values (12, 'Mercedes', '11111', 'Mercedes_11111_it');
-insert into car(id, brand, code, description) values (2, 'BMW', '99999', 'BMW_99999_en');
-insert into car(id, brand, code, description) values (3, 'Ford', '22222', 'Ford_22222_it');
-insert into car(id, brand, code, description) values (4, 'BMW', '88888', 'BMW_88888_de');
-insert into car(id, brand, code, description) values (5, 'Mercedes', '33333', 'Mercedes_33333_en');
-insert into car(id, brand, code, description) values (6, 'Ford', '44444', 'Ford_44444_de');
+insert into car(id, brand, code) values (1, 'Mercedes', '11111');
+insert into car(id, brand, code) values (11, 'Mercedes', '11111');
+insert into car(id, brand, code) values (12, 'Mercedes', '11111');
+insert into car(id, brand, code) values (2, 'BMW', '99999');
+insert into car(id, brand, code) values (3, 'Ford', '22222');
+insert into car(id, brand, code) values (4, 'BMW', '88888');
+insert into car(id, brand, code) values (5, 'Mercedes', '33333');
+insert into car(id, brand, code) values (6, 'Ford', '44444');
