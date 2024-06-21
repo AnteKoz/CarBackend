93f552ac209162d63e582bcb67cfb0f50806a13d
AnteKoz <Ante.Kozina@adesso.de>
2024-06-21 15:43:42 +0200

    feat: Updating flyway script of CarData

--- src/main/resources/db/migration/V1__CreateTableCarData.sql
+++ src/main/resources/db/migration/V1__CreateTableCarData.sql
@@ -1,5 +1,7 @@
 CREATE TABLE IF NOT EXISTS car(
     id BIGINT PRIMARY KEY,
     brand VARCHAR(255),
+    type VARCHAR(255),
+    model VARCHAR(255),
     code VARCHAR(255)
 );
--- src/main/resources/db/migration/V2__TestData.sql
+++ src/main/resources/db/migration/V2__TestData.sql
@@ -1,8 +1,8 @@
-insert into car(id, brand, code) values (1, 'Mercedes', '11111');
-insert into car(id, brand, code) values (11, 'Mercedes', '11111');
-insert into car(id, brand, code) values (12, 'Mercedes', '11111');
-insert into car(id, brand, code) values (2, 'BMW', '99999');
-insert into car(id, brand, code) values (3, 'Ford', '22222');
-insert into car(id, brand, code) values (4, 'BMW', '88888');
-insert into car(id, brand, code) values (5, 'Mercedes', '33333');
-insert into car(id, brand, code) values (6, 'Ford', '44444');
+insert into car(id, brand, type, model, code) values (1, 'Mercedes', 'PKW', 'C-Klasse', '11111');
+insert into car(id, brand, type, model, code) values (11, 'Mercedes', 'PKW', 'S-Klasse', '21211');
+insert into car(id, brand, type, model, code)  values (12, 'Mercedes', 'PKW', 'A-Klasse', '11191');
+insert into car(id, brand, type, model, code)  values (2, 'BMW', 'PKW', '3 Series', '99999');
+insert into car(id, brand, type, model, code)  values (3, 'Ford', 'PKW', 'FOCUS', '22222');
+insert into car(id, brand, type, model, code)  values (4, 'BMW', 'PKW', '2 Series', '88888');
+insert into car(id, brand, type, model, code)  values (5, 'Mercedes', 'PKW', 'B-Klasse', '33333');
+insert into car(id, brand, type, model, code)  values (6, 'Ford', 'PKW', 'Ranger', '44444');
